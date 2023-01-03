c7acdac554b7587577f59ac3767b1e3fcf1b8f5b
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-01-03 16:10:36 -0700

    Built out file structure and got homepage running on localhost

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require('./User.js');
+const Post = require('./Post.js');
+const Comment = require('./Comment.js');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { Comment, Post, User };
