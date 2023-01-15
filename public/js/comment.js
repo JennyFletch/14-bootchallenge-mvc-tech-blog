537e40d04724504a9f8ec8f9f09b5f2bd034c4c9
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-01-15 16:40:24 -0700

    Added comment buttons to homepage with simple passing of post id

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,10 +1,12 @@
-function commentOnPost(e) {
-    // console.log(e.target.getAttribute('data-id'));
-    var comPostId = e.target.getAttribute('data-id')
+const commentOnPost = async (e) => {
+    
+  e.stopPropagation();
+  var comPostId = e.target.getAttribute('data-id');
+  console.log("Need to comment on post #" + comPostId);
 
 }
 
-var selectedPost = document.querySelector('.blogroll-posts');
-if(selectedPost) {
-  selectedPost.addEventListener('click', commentOnPost);
+var homePosts = document.querySelector('.blogroll-posts-home');
+if(homePosts) {
+  homePosts.addEventListener('click', commentOnPost);
 } 
